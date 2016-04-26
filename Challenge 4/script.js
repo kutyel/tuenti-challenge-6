6a76f370009c9f68ba019bf3caa282c7d16ffe02
kutyel <flaviocorpa@gmail.com>
2016-04-26 19:13:02 +0200

    Skip Challenge 4 :(

--- Challenge 4/script.js
+++ Challenge 4/script.js
@@ -5,38 +5,50 @@ const fs = require('fs');
 fs.unlink('./output.txt');
 
 const incompleteCombos = [
-    ['L-LD-D-RD-R'],
-    ['D-RD-R'],
-    ['R-D-RD'],
-    ['D-LD-L'],
-    ['R-RD-D-LD-L']
+    'L-LD-D-RD-R',
+         'D-RD-R',
+         'R-D-RD',
+         'D-LD-L',
+    'R-RD-D-LD-L'
 ];
 
 const completeCombos = [
-    ['L-LD-D-RD-R-P'],
-    ['D-RD-R-P'],
-    ['R-D-RD-P'],
-    ['D-LD-L-K'],
-    ['R-RD-D-LD-L-K']
+    'L-LD-D-RD-R-P',
+         'D-RD-R-P',
+         'R-D-RD-P',
+         'D-LD-L-K',
+    'R-RD-D-LD-L-K'
 ];
 
 let cases = 0, t = 0;
 
-fs.readFileSync('./input.txt').toString().split('\n').forEach(line => {
+fs.readFileSync('./testInput.txt').toString().split('\n').forEach(line => {
 
     if (t === 0) {
         cases = line;
     } else if (t <= cases) {
 
-        let r = 0;
+        let r = 0, aux = {};
 
         incompleteCombos.forEach((x, i) => {
             let partialMatches = (line.match(new RegExp(x, 'g')) || []).length;
             if (partialMatches) {
                 let completeMatches = (line.match(new RegExp(completeCombos[i], 'g')) || []).length;
-                r += (partialMatches - completeMatches);
+                if (partialMatches - completeMatches) {
+                    aux[x] = partialMatches - completeMatches;
+                }
             }
         });
+
+        for (let a in aux) {
+            if (a === incompleteCombos[1] && aux.hasOwnProperty(incompleteCombos[0])) {
+                r += (aux[a] - aux[incompleteCombos[0]]);
+            } else if (a === incompleteCombos[3] && aux.hasOwnProperty(incompleteCombos[4])) {
+                r += (aux[a] - aux[incompleteCombos[4]]);
+            } else {
+                r += aux[a];
+            }
+        }
 
         const result = `Case #${t}: ${r}`;
 
