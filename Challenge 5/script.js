0aa0f05c43fd7977f583f655b7b4a727d5c8b02c
Flavio Corpa <flaviocorpa@gmail.com>
2016-04-27 12:51:27 +0200

    POST to "Hangman" challenge...

--- Challenge 5/script.js
+++ Challenge 5/script.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const fs = require('fs');
+const unirest = require('unirest');
 
 fs.unlink('./output.txt');
 
-const url = '52.49.91.111:9988';
+const url = 'http://52.49.91.111:9988';
 
 const words = fs.readFileSync('./words.txt').toString().split('\n');
 
-const possibility = words.filter(x => x.length === 4);
-
-// TODO: curl very possibility to the "Hangman"" service
-
-fs.appendFileSync('./output.txt', possibility.join('\n'));
+words.filter(x => x.length === 4).forEach(x =>
+    unirest.post(url)
+        .header('Accept', 'application/json')
+        .send({ 'word': x })
+        .end(response => {
+            if (!response.error) {
+                fs.appendFileSync('./output.txt', response.body);
+            }
+        })
+);
