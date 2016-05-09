72782c753ea6e46e1395c8404458e1a4608e5cde
kutyel <flaviocorpa@gmail.com>
2016-05-09 20:44:35 +0200

    Solve Challenge 9 :)

--- Challenge 9/script.js
+++ Challenge 9/script.js
@@ -4,27 +4,35 @@ const fs = require('fs');
 
 fs.unlink('./output.txt');
 
-function isImmiscible(number) {
-    return /^1{1,}0{0,}$/.test(number.toString());
-}
-
 let cases = 0;
 
 fs.readFileSync('./testInput.txt').toString().split('\n').forEach((num, t) => {
 
-    let m = 1, ones = 0, zeroes = 0;
+    let m = +num, fact2 = 0, fact5 = 0, mod = 0, ones = 1, zeroes = 0;
 
     if (t === 0) {
-        cases = +num;
+        cases = m;
     } else if (t <= cases) {
 
-        while (!isImmiscible(+num * m)) {
-            m++;
+        // the factors 2 and 5 are satisfied using zeros
+        while (m % 2 === 0) {
+            fact2++;
+            m /= 2;
+        }
+        while (m % 5 === 0) {
+            fact5++;
+            m /= 5;
         }
 
-        let r = +num * m;
-        ones = r.toString().split('').filter(x => x === '1').length;
-        zeroes = r.toString().split('').filter(x => x === '0').length;
+        zeroes = Math.max(fact2, fact5);
+
+        // the rest of the factors are satisfied using ones
+        mod = 1 % m;
+
+        while (mod !== 0) {
+            mod = (10 * mod + 1) % m;
+            ones++;
+        }
 
         const result = `Case #${t}: ${ones} ${zeroes}`;
 
