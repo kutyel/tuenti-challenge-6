889110347799dd2a93219d26cb45f1408aebb49e
Flavio Corpa <flaviocorpa@gmail.com>
2016-06-16 08:33:19 +0200

    More functional

--- Challenge 2/script.py
+++ Challenge 2/script.py
@@ -3,21 +3,21 @@ import operator
 
 corpus = open('corpus.txt', 'r').readline().split(' ')
 output = open('output.txt', 'w')
-input = open('submitInput.txt', 'r')
+input = open('testInput.txt', 'r')
 cases = int(input.readline())
 lines = input.readlines()
 
 for test, line in enumerate(lines):
-    result = {}
+    results = {}
     [start, end] = line.split(' ')
 
     for i in range(int(start), int(end)):
         word = corpus[i]
-        if word in result:
-            result[word] += 1
+        if word in results:
+            results[word] += 1
         else:
-            result[word] = 1
+            results[word] = 1
 
-    top = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
+    top = sorted(results.items(), key=operator.itemgetter(1), reverse=True)
 
-    print("Case #{0}: {1} {2},{3} {4},{5} {6}".format(test, top[0][0], top[0][1], top[1][0], top[1][1], top[2][0], top[2][1]), file=output)
+    print("Case #{0}: {1}".format(test+1, ",".join(map((lambda x: "{0} {1}".format(x[0], x[1])), top[:3]))), file=output)
