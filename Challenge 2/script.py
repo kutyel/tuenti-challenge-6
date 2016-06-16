986e4a81f4d002a4588d4628a90a1ef4a6db605a
Flavio Corpa <flaviocorpa@gmail.com>
2016-06-16 08:15:48 +0200

    More "Pythonic" :snake:

--- Challenge 2/script.py
+++ Challenge 2/script.py
@@ -1,17 +1,17 @@
+from __future__ import print_function
 import operator
 
 corpus = open('corpus.txt', 'r').readline().split(' ')
+output = open('output.txt', 'w')
 input = open('submitInput.txt', 'r')
 cases = int(input.readline())
 lines = input.readlines()
-test = 1
 
-for line in lines:
+for test, line in enumerate(lines):
     result = {}
-    start = int(line.split(' ')[0])
-    end = int(line.split(' ')[1])
+    [start, end] = line.split(' ')
 
-    for i in range(start, end):
+    for i in range(int(start), int(end)):
         word = corpus[i]
         if word in result:
             result[word] += 1
@@ -20,6 +20,4 @@ for line in lines:
 
     top = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
 
-    print "Case #%d: %s %d,%s %d,%s %d" % (test, top[0][0], top[0][1], top[1][0], top[1][1], top[2][0], top[2][1])
-    
-    test += 1
+    print("Case #{0}: {1} {2},{3} {4},{5} {6}".format(test, top[0][0], top[0][1], top[1][0], top[1][1], top[2][0], top[2][1]), file=output)
