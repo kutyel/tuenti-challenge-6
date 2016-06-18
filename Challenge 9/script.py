a4f43b10d17fbd650f4047bde563b4799481ec68
kutyel <flaviocorpa@gmail.com>
2016-06-18 20:15:35 +0200

    Some corrections

--- Challenge 9/script.py
+++ Challenge 9/script.py
@@ -1,29 +1,29 @@
 from __future__ import print_function
 
-outpt = open('output.txt', 'w')
-input = open('testInput.txt', 'r')
-cases = int(input.readline())
-lines = input.readlines()
+with open('output.txt', 'w') as output:
 
-for test, line in enumerate(lines):
-    ones = 1
-    fact2 = 0
-    fact5 = 0
-    num = int(line)
+    with open('testInput.txt', 'r') as input_:
+        cases = int(input_.readline())
+        lines = input_.readlines()
 
-    while num % 2 == 0:
-        fact2 += 1
-        num /= 2
+        for test, line in enumerate(lines):
+            ones, fact2, fact5 = 1, 0, 0
+            num = int(line)
 
-    while num % 5 == 0:
-        fact5 += 1
-        num /= 5
+            while num % 2 == 0:
+                fact2 += 1
+                num /= 2
 
-    zeroes = max(fact2, fact5)
-    mod = 1 % num
+            while num % 5 == 0:
+                fact5 += 1
+                num /= 5
 
-    while mod != 0:
-        mod = (10 * mod + 1) % num
-        ones += 1
+            zeroes = max(fact2, fact5)
+            mod = 1 % num
 
-    print("Case #{0}: {1} {2}".format(test+1, ones, zeroes), file=outpt)
+            while mod:
+                mod = (10 * mod + 1) % num
+                ones += 1
+
+            print("Case #{0}: {1} {2}".format(test+1, ones, zeroes),
+                file=output)
