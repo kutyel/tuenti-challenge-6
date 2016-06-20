52610e7273b2d6ef6b4bc30f216b3c670ed14dd9
Flavio Corpa <flaviocorpa@gmail.com>
2016-06-20 08:18:30 +0200

    Corrections by @yajo :wink:

--- Challenge 1/script.py
+++ Challenge 1/script.py
@@ -1,19 +1,20 @@
 from __future__ import print_function
 
-outpt = open('output.txt', 'w')
-input = open('submitInput.txt', 'r')
-cases = int(input.readline())
-lines = input.readlines()
+output = open('output.txt', 'w')
 
-for test, line in enumerate(lines):
-    result = 0
-    people = int(line)
+with open('submitInput.txt', 'r') as input_:
+    cases = int(input_.readline())
+    lines = input_.readlines()
 
-    if people == 4:
-        result = 1
-    else:
-        while people > 0:
-            people -= 4 if result < 1 else 2
-            result += 1
+    for test, line in enumerate(lines):
+        result = 0
+        people = int(line)
 
-    print("Case #{0}: {1}".format(test, result), file=outpt)
+        if people == 4:
+            result = 1
+        else:
+            while people > 0:
+                people -= 4 if result < 1 else 2
+                result += 1
+
+        print("Case #{0}: {1}".format(test+1, result), file=output)
