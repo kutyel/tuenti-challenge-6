1ba385265e21b829e3b57a1487224cc28979204e
kutyel <flaviocorpa@gmail.com>
2016-06-20 22:09:04 +0200

    Leave just one `with`

--- Challenge 9/script.py
+++ Challenge 9/script.py
@@ -1,29 +1,28 @@
 from __future__ import print_function
 
-with open('output.txt', 'w') as output:
+output =  open('output.txt', 'w')
 
-    with open('testInput.txt', 'r') as input_:
-        cases = int(input_.readline())
-        lines = input_.readlines()
+with open('testInput.txt', 'r') as input_:
+    cases = int(input_.readline())
+    lines = input_.readlines()
 
-        for test, line in enumerate(lines):
-            ones, fact2, fact5 = 1, 0, 0
-            num = int(line)
+    for test, line in enumerate(lines):
+        ones, fact2, fact5 = 1, 0, 0
+        num = int(line)
 
-            while num % 2 == 0:
-                fact2 += 1
-                num /= 2
+        while num % 2 == 0:
+            fact2 += 1
+            num /= 2
 
-            while num % 5 == 0:
-                fact5 += 1
-                num /= 5
+        while num % 5 == 0:
+            fact5 += 1
+            num /= 5
 
-            zeroes = max(fact2, fact5)
-            mod = 1 % num
+        zeroes = max(fact2, fact5)
+        mod = 1 % num
 
-            while mod:
-                mod = (10 * mod + 1) % num
-                ones += 1
+        while mod:
+            mod = (10 * mod + 1) % num
+            ones += 1
 
-            print("Case #{0}: {1} {2}".format(test+1, ones, zeroes),
-                file=output)
+        print("Case #{0}: {1} {2}".format(test+1, ones, zeroes), file=output)
