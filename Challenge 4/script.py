744389e8e55c48acd95058456fe5ab56bf34d93c
Flavio Corpa <flaviocorpa@gmail.com>
2016-06-21 08:00:50 +0200

    Add context manager `with`

--- Challenge 4/script.py
+++ Challenge 4/script.py
@@ -1,61 +1,60 @@
 from __future__ import print_function
 
-outpt = open('output.txt', 'w')
-input = open('testInput.txt', 'r')
-cases = int(input.readline())
-lines = input.readlines()
-combos = [
-    "-D-RD-R-P-",
-    "-R-D-RD-P-",
-    "-D-LD-L-K-"
-]
-breaks = [
-    "-D-RD-R-",
-    "-R-D-RD-",
-    "-D-LD-L-"
-]
+output = open('output.txt', 'w')
 
-for test, line in enumerate(lines):
-    result = 0
-    offset = 0
-    last10 = ""
-    moves = "-" + line + "-"
-    possible_combo = False
+with open('testInput.txt', 'r') as input_:
+    cases = int(input_.readline())
+    lines = input_.readlines()
+    combos = [
+        "-D-RD-R-P-",
+        "-R-D-RD-P-",
+        "-D-LD-L-K-"
+    ]
+    breaks = [
+        "-D-RD-R-",
+        "-R-D-RD-",
+        "-D-LD-L-"
+    ]
 
-    while offset < len(moves):
-        move = moves[offset:offset+2]
-        offset += 2
-
-        if len(move) > 1 and move[1] != "-":
-            move += "-"
-            offset += 1
-
-        last10 += move
-        current_length = len(last10)
-
-        if current_length > 10:
-            last10 = last10[current_length-10:]
-            current_length = 10
-
-        has_combo = False
-
-        for c in combos:
-            if c == last10:
-                has_combo = True
-                break
-
-        if not has_combo and possible_combo:
-            result += 1
-
+    for test, line in enumerate(lines):
+        result, offset = 0, 0
+        last10, moves = "", "-{0}-".format(line)
         possible_combo = False
 
-        if current_length >= 8:
-            for b in breaks:
-                if b == last10[current_length-8:]:
-                    possible_combo = True
+        while offset < len(moves):
+            move = moves[offset:offset+2]
+            offset += 2
+
+            if len(move) > 1 and move[1] != "-":
+                move += "-"
+                offset += 1
+
+            last10 += move
+            current_length = len(last10)
+
+            if current_length > 10:
+                last10 = last10[current_length-10:]
+                current_length = 10
+
+            has_combo = False
+
+            for c in combos:
+                if c == last10:
+                    has_combo = True
                     break
 
-    if possible_combo:
-        result += 1
+            if not has_combo and possible_combo:
+                result += 1
 
-    print("Case #{0}: {1}".format(test, result), file=outpt)
+            possible_combo = False
+
+            if current_length >= 8:
+                for b in breaks:
+                    if b == last10[current_length-8:]:
+                        possible_combo = True
+                        break
+
+        if possible_combo:
+            result += 1
+
+        print("Case #{0}: {1}".format(test + 1, result), file=output)
