bf67c3e7093e86c31a55efd73085498489f67580
kutyel <flaviocorpa@gmail.com>
2016-06-28 22:33:50 +0200

    Closer to solution :rocket:

--- Challenge 5/script.py
+++ Challenge 5/script.py
@@ -1,21 +1,22 @@
 from __future__ import print_function
+import re
 import socket
 
+words = [word for word in open("words.txt") if len(word) == 5]
+
 with open("output.txt", "w") as output:
-
-    with open("words.txt", "r") as dictionary:
-        words = dictionary.readlines()
-        sock = socket.socket()
-        sock.connect(("52.49.91.111", 9988))
-
-        try:
-            while True:
+    sock = socket.socket()
+    sock.connect(("52.49.91.111", 9988))
+    try:
+        while True:
+            # press enter to continue
+            sock.send("\n")
+            for word in words:
+                sock.send(word)
                 response = sock.recv(1024)
                 print(response)
-                for word in words:
-                    sock.send(word)
-        except socket.timeout:
-            print("Game Over")
-            sock.close()
-
-        print("result", file=output)
+                # TODO: use this regex to filter words
+                print(re.findall("_*\s*\w*", response))
+    except socket.error as e:
+        print("Error: {0}".format(e))
+        sock.close()
